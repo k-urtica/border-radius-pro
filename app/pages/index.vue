566fb5c1c44d18dc79e07d898c9ed49453b78a9c
K <k.rev.urtica@gmail.com>
2025-08-09 07:16:00 +0900

    fix: add min-w-0

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,7 +9,7 @@ useSeoMeta({
 
 <template>
   <div class="flex h-(--viewport-content-height) flex-row gap-2.5 p-2">
-    <main class="flex flex-1 flex-row gap-2.5">
+    <main class="flex min-w-0 flex-1 flex-row gap-2.5">
       <ControlPanel class="hidden lg:block" />
       <div class="flex min-w-0 flex-1 flex-col gap-2 lg:block">
         <ContentPanel v-show="!isDocVisible" />
