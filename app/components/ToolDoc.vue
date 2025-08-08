c44595bdacc0e7a57c83e5d13a8448fb5909db88
K <46732829+k-urtica@users.noreply.github.com>
2025-08-08 16:31:08 +0900

    feat: add tool guide (#3)

--- app/components/ToolDoc.vue
+++ app/components/ToolDoc.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
-// import '@/assets/markdown.css';
+import markdownContent from '@/docs/guide.mdc?raw';
+import '@/assets/markdown.css';
 
 const { toggleDoc } = useDocToggle();
-// const { locale: currentLocale } = useI18n();
-
-// const { data: doc } = await useAsyncData(
-//   `docs-${currentLocale.value}`,
-//   () => queryCollection('docs').where('stem', '=', `docs/${currentLocale.value}`).first()
-// );
 </script>
 
 <template>
@@ -26,12 +21,7 @@ const { toggleDoc } = useDocToggle();
       </nav>
 
       <Motion v-bind="fade()">
-        <!-- <ContentRenderer
-          v-if="doc"
-          :value="doc.meta"
-          class="markdown-body"
-          tag="section"
-        /> -->
+        <MDC :value="markdownContent" tag="section" class="markdown-body" />
       </Motion>
     </article>
   </PanelContainer>
